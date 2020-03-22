c29a66de77603b55ef5a870b78c547d3ffd33dfa
omjadas <omja.das@gmail.com>
2020-03-22 14:19:26 +1100

    implmenet Agenda

--- src/Hall.java
+++ src/Hall.java
@@ -3,6 +3,13 @@
  */
 
 public class Hall {
-    public Hall(String string, Agenda agendaNew, Agenda agendaComplete) {
+    private String name;
+    private Agenda agendaNew;
+    private Agenda agendaComplete;
+
+    public Hall(String name, Agenda agendaNew, Agenda agendaComplete) {
+        this.name = name;
+        this.agendaNew = agendaNew;
+        this.agendaComplete = agendaComplete;
     }
 }
--- src/Knight.java
+++ src/Knight.java
@@ -3,10 +3,24 @@
  */
 
 public class Knight extends Thread {
+    private int id;
+    private Agenda agendaNew;
+    private Agenda agendaComplete;
+    private Hall greatHall;
+
     public Knight(
-            int i,
+            int id,
             Agenda agendaNew,
             Agenda agendaComplete,
             Hall greatHall) {
+        this.id = id;
+        this.agendaNew = agendaNew;
+        this.agendaComplete = agendaComplete;
+        this.greatHall = greatHall;
+    }
+
+    public void run() {
+        while (!isInterrupted()) {
+        }
     }
 }
